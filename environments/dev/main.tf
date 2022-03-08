dd1fe9f71b1ce9252ddd80d4b287260763ea6c60
Kumari Renuka <90672919+renuka-kumari@users.noreply.github.com>
2022-03-08 20:45:21 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#Comments
+
 module "vpc" {
   source  = "../../modules/vpc"
   project = "${var.project}"
